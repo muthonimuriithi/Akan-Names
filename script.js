0d64c92a27f797a1fe5727495bfdbcd6abf3405d
muthonimuriithi <loissmuthoni@student.moringaschool.com>
2022-03-07 05:37:54 +0300

    added an alert button to the output of the Akan name

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
-let form=document.getElementById("form");
-let date=document.forms["form"]["day"];
-let month=document.forms["month"]["month"];
-let year=document.forms["year"]["year"];
+// let form=document.getElementById("form");
+// let date=document.forms["form"]["day"];
+// let month=document.forms["month"]["month"];
+// let year=document.forms["year"]["year"];
 
-function myButton(){
-    if(date.value.length=="" || month.value.length=="" || year.value.length==""){
-        // document.getElementById("").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
-        alert("kindlu fii all the fields");
-        return false;
+// function myButton(){
+//     if(date.value.length=="" || month.value.length=="" || year.value.length==""){
+//         // document.getElementById("").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
+//         alert("kindlu fii all the fields");
+//         return false;
+//     }
+// }
+var get = function (y, m, d, h) {
+    var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleName = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var d = new Date(y, --m, d);
+
+    if (h === "female") {
+        return d && femaleName[d.getDay()];
+    } else {
+        return d && maleName[d.getDay()];
     }
+}
+
+$(document).ready(function () {
+    $("form#form").submit(function (event) {
+        event.preventDefault();
+        var y = parseInt($("#year").val());
+        var d = parseInt($("#date").val());
+        var m = parseInt($("#month").val());
+        var h = $("input:radio[name=gender]:checked").val();
+        var result = get(y, m, d, h);
+        alert("Your Akan Name is: " + result);
+
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
